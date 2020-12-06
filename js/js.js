bd25280a9c866e3feeda7e6e55155e075d068e09
hendorism <hendorism@gmail.com>
2020-12-06 15:57:36 -0800

    started repo
    made basic file structure for javascript app in static page

---
+++ js/js.js
@@ -0,0 +1,7 @@
+// Aaron Dull 2020 12 06
+// full-scale Solar System gravity physics sim
+// not on the canvas, but in the DOM!!!
+
+// function zoom() {zoom in and out}
+
+// function solarSystemObject(x, y, z, vx, vy, vz, mass, density) {create object with initial velocity and position}
