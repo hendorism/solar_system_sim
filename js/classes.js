14f73df8532a90fe04ca794938267d30cd547a52
hendorism <hendorism@gmail.com>
2020-12-29 17:49:26 -0800

    dfgdfg

---
+++ js/classes.js
@@ -0,0 +1,9 @@
+class Planet {
+  constructor(x, y, z, v, d) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.v = v;
+    this.d = d;
+  }
+}
--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 // Aaron Dull 2020 12 06
 // full-scale Solar System gravity physics sim
 // not on the canvas, but in the DOM!!!
@@ -5,3 +6,15 @@
 // function zoom() {zoom in and out}
 
 // function solarSystemObject(x, y, z, vx, vy, vz, mass, density) {create object with initial velocity and position}
+
+
+
+
+
+
+
+
+
+
+
+  });
